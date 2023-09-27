5c51dd3b6d55533e34093b4a17e0d82f35fb8528
Jedrzej Kosinski <kosinkadink1@gmail.com>
2023-09-26 19:53:35 -0500

    changed LoadVideo node to use cv to support frame_load_cap and skip_first_frames + additional formats, added frame_count output, changed visual appearance of nodes to make it clear what custom nodes they are part of

--- videohelpersuite/nodes.py
+++ videohelpersuite/nodes.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 import json
 import subprocess
 import shutil
@@ -7,8 +8,9 @@ import time
 import numpy as np
 from typing import List
 import torch
-from PIL import Image
+from PIL import Image, ImageOps
 from PIL.PngImagePlugin import PngInfo
+import cv2
 
 import folder_paths
 from .logger import logger
@@ -16,6 +18,7 @@ from .logger import logger
 ffmpeg_path = shutil.which("ffmpeg")
 if ffmpeg_path is None:
     logger.warning("ffmpeg could not be found. Outputs that require it have been disabled")
+
 
 class VideoCombine:
     @classmethod
@@ -30,7 +33,7 @@ class VideoCombine:
                 "images": ("IMAGE",),
                 "frame_rate": (
                     "INT",
-                    {"default": 8, "min": 1, "max": 24, "step": 1},
+                    {"default": 8, "min": 1, "step": 1},
                 ),
                 "loop_count": ("INT", {"default": 0, "min": 0, "max": 100, "step": 1}),
                 "filename_prefix": ("STRING", {"default": "AnimateDiff"}),
@@ -46,7 +49,7 @@ class VideoCombine:
 
     RETURN_TYPES = ("GIF",)
     OUTPUT_NODE = True
-    CATEGORY = "Video Helper Suite"
+    CATEGORY = "Video Helper Suite 🎥🅥🅗🅢"
     FUNCTION = "combine_video"
 
     def save_with_tempfile(self, args, metadata, file_path, frames, env):
@@ -204,7 +207,7 @@ class VideoCombine:
 class LoadVideo:
     @classmethod
     def INPUT_TYPES(s):
-        video_extensions = ['webm', 'mp4', 'mkv']
+        video_extensions = ['webm', 'mp4', 'mkv', 'gif']
         input_dir = folder_paths.get_input_directory()
         files = []
         for f in os.listdir(input_dir):
@@ -212,19 +215,70 @@ class LoadVideo:
                 file_parts = f.split('.')
                 if len(file_parts) > 1 and (file_parts[-1] in video_extensions):
                     files.append(f)
-        return {"required":
-                    {"video": (sorted(files), {"video_upload": True}),
-                     "force_rate": ("INT", {"default": 0, "min": 0, "max": 24, "step": 1}),
+        return {"required": {
+                    "video": (sorted(files), {"video_upload": True}),
+                     #"force_rate": ("INT", {"default": 0, "min": 0, "max": 24, "step": 1}),
                      "force_size": (["Disabled", "256x?", "?x256", "256x256", "512x?", "?x512", "512x512"],),
+                     "frame_load_cap": ("INT", {"default": 0, "min": 0, "step": 1}),
+                     "skip_first_frames": ("INT", {"default": 0, "min": 0, "step": 1}),
                      #Consider adding start_frame/total_frames here?
                      #Might be a bit finicky since ffmpeg usually works in time/duration, not frame numbers
                      },}
 
-    CATEGORY = "Video Helper Suite"
+    CATEGORY = "Video Helper Suite 🎥🅥🅗🅢"
 
-    RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "load_video"
-    def load_video(self, video, force_rate, force_size):
+    RETURN_TYPES = ("IMAGE", "INT",)
+    RETURN_NAMES = ("IMAGE", "frame_count",)
+    FUNCTION = "load_video_cv_fallback"
+
+    def is_gif(self, filename):
+        file_parts = filename.split('.')
+        return len(file_parts) > 1 and file_parts[-1] == "gif"
+
+    def load_video_cv_fallback(self, video, force_size, frame_load_cap, skip_first_frames):
+        try:
+            video_cap = cv2.VideoCapture(folder_paths.get_annotated_filepath(video))
+            if not video_cap.isOpened():
+                raise ValueError(f"{video} could not be loaded with cv fallback.")
+            # set video_cap to look at start_index frame
+            images = []
+            total_frame_count = 0
+            frames_added = 0
+            while video_cap.isOpened():
+                # if cap exists and we've reached it, stop processing frames
+                if frame_load_cap > 0 and frames_added >= frame_load_cap:
+                    break
+                is_returned, frame = video_cap.read()
+                # if didn't return frame, video has ended
+                if not is_returned:
+                    break 
+                # if not at start_index, skip doing anything with frame
+                total_frame_count += 1
+                if total_frame_count <= skip_first_frames:
+                    continue
+                # TODO: do whatever operations need to happen, like force_size, etc
+
+                # opencv loads images in BGR format (yuck), so need to convert to RGB for ComfyUI use
+                # follow up: can videos ever have an alpha channel?
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # convert frame to comfyui's expected format (taken from comfy's load image code)
+                image = Image.fromarray(frame)
+                image = ImageOps.exif_transpose(image)
+                image = np.array(image, dtype=np.float) / 255.0
+                image = torch.from_numpy(image)[None,]
+                images.append(image)
+                frames_added += 1
+        finally:
+            video_cap.release()
+        # TODO: raise an error maybe if no frames were loaded?
+        return (torch.cat(images, dim=0), frames_added)
+
+    def load_video(self, video, force_rate, force_size, frame_load_cap, skip_first_frames):
+        # check if video is a gif - will need to use cv fallback to read frames
+        # use cv fallback if ffmpeg not installed or gif
+        if ffmpeg_path is None or self.is_gif(video):
+            return self.load_video_cv_fallback(video, force_size, frame_load_cap, skip_first_frames)
+        # otherwise, continue with ffmpeg
         video_path = folder_paths.get_annotated_filepath(video)
         args_dummy = ["ffmpeg", "-i", video_path, "-f", "null", "-"]
         with subprocess.Popen(args_dummy, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE) as proc:
@@ -275,18 +329,18 @@ class LoadVideo:
                 logger.warn(f'{current_offset} bytes left over when loading image')
 
         images = torch.from_numpy(np.stack(images))
-        return (images,)
+        return (images, images.size(0))
 
     @classmethod
-    def IS_CHANGED(s, image):
-        image_path = folder_paths.get_annotated_filepath(image)
+    def IS_CHANGED(s, video, force_size, frame_load_cap, skip_first_frames):
+        image_path = folder_paths.get_annotated_filepath(video)
         m = hashlib.sha256()
         with open(image_path, 'rb') as f:
             m.update(f.read())
         return m.digest().hex()
 
     @classmethod
-    def VALIDATE_INPUTS(s, video, force_rate, force_size):
+    def VALIDATE_INPUTS(s, video, force_size, frame_load_cap, skip_first_frames):
         if not folder_paths.exists_annotated_filepath(video):
             return "Invalid image file: {}".format(video)
 
@@ -297,6 +351,6 @@ NODE_CLASS_MAPPINGS = {
     "VHS_LoadVideo": LoadVideo,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "VHS_VideoCombine": "Video Combine",
-    "VHS_LoadVideo": "Load Video",
+    "VHS_VideoCombine": "Video Combine 🎥🅥🅗🅢",
+    "VHS_LoadVideo": "Load Video 🎥🅥🅗🅢",
 }
