4830e1e89299f6de233a7c9dce963cff6412f925
Austin Mroz <AustinMroz@utexas.edu>
2025-09-17 12:31:54 -0500

    Allow shift+tab to edit previous widget

--- web/js/VHS.core.js
+++ web/js/VHS.core.js
@@ -1753,13 +1753,12 @@ function mouseAnnotated(event, [x, y], node) {
                     d_callback(input.value)
                     dialog.close()
                     node?.graph?.setDirtyCanvas(true);
-                    let i=0
-                    for (;i<node.widgets.length;i++) {
-                        if (node.widgets[i] == this) {
-                            break
-                        }
-                    }
-                    if (node.widgets[++i]?.type == "VHS.ANNOTATED") {//restrict to annotatedNUmbers
+                    let i = node.widgets.findIndex((w) => w == this)
+                    if (e.shiftKey)
+                        i--
+                    else
+                        i++
+                    if (node.widgets[i]?.type == "VHS.ANNOTATED") {//restrict to annotatedNUmbers
                         node.widgets[i]?.mouse(event, [x, y+24], node)
                     }
                 }
