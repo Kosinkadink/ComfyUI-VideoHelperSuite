02dfc5ee26ceaaabd4c4dbff53e2075e258a626f
Austin Mroz <AustinMroz@utexas.edu>
2026-08-24 21:27:50 -0700

    Move unbatch to matchtype
    VHS is v3 now?

--- web/js/VHS.core.js
+++ web/js/VHS.core.js
@@ -579,52 +579,6 @@ function addVAEInputToggle(nodeType, nodeData) {
         }
     });
 }
-function cloneType(nodeType, nodeData) {
-    chainCallback(nodeType.prototype, "onNodeCreated", function() {
-        this.changeOutputType = function (new_type) {
-            this.linkTimeout = setTimeout(() => {
-                this.linkTimeout = false
-                if (this.outputs[0].type != new_type) {
-                    this.outputs[0].type = new_type
-                    //check and potentially remove links
-                    if (!this.outputs[0].links) {
-                        return
-                    }
-                    let removed_links = []
-                    for (let link_id of this.outputs[0].links) {
-                        let link = app.graph.links[link_id]
-                        if (!link)
-                            debugger
-                        let target_node = app.graph.getNodeById(link.target_id)
-                        let target_input = target_node.inputs[link.target_slot]
-                        let keep = LiteGraph.isValidConnection(new_type, target_input.type)
-                        if (!keep) {
-                            link.disconnect(app.graph, 'input')
-                            removed_links.push(link_id)
-                        }
-                        target_node.onConnectionsChange?.(LiteGraph.INPUT,
-                            link.target_slot, keep, link, target_input)
-                    }
-                    this.outputs[0].links = this.outputs[0].links
-                        .filter((v) => !removed_links.includes(v))
-                }
-            }, 50)
-        }
-        this.changeOutputType("VHS_DUMMY_NONE")
-    });
-    chainCallback(nodeType.prototype, "onConnectionsChange", function(contype, slot, iscon, linf) {
-        if (contype == LiteGraph.INPUT && slot == 0) {
-            let new_type = "VHS_DUMMY_NONE"
-            if (iscon && linf) {
-                new_type = app.graph.getNodeById(linf.origin_id).outputs[linf.origin_slot].type
-            }
-            if (this.linkTimeout) {
-                clearTimeout(this.linkTimeout)
-            }
-            this.changeOutputType(new_type)
-        }
-    });
-}
 
 function addDateFormatting(nodeType, field, timestamp_widget = false) {
     chainCallback(nodeType.prototype, "onNodeCreated", function() {
@@ -2075,8 +2029,6 @@ app.registerExtension({
                     computeSize: () => {return [0,-4]},
                     afterQueued: function() {this.value++;}});
             });
-        } else if (nodeData?.name == "VHS_Unbatch") {
-            cloneType(nodeType, nodeData)
         } else if (nodeData?.name == "VHS_SelectLatest") {
             chainCallback(nodeType.prototype, "onNodeCreated", function() {
                 this.isVirtualNode = true
