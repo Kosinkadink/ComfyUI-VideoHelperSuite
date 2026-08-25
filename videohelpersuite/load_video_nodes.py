593fcb07e6ccbaad7112413d4b47cf7be286b56f
Austin Mroz <AustinMroz@utexas.edu>
2026-08-24 21:07:57 -0700

    Support h3 as input format

--- videohelpersuite/load_video_nodes.py
+++ videohelpersuite/load_video_nodes.py
@@ -29,6 +29,7 @@ VHSLoadFormats = {
     'Hunyuan': {'target_rate': 24, 'dim': (16,0,848,480), 'frames':(4,1)},
     'Cosmos': {'target_rate': 24, 'dim': (16,0,1280,704), 'frames':(8,1)},
     'Wan': {'target_rate': 16, 'dim': (8,0,832,480), 'frames':(4,1)},
+    'H3': {'target_rate': 24, 'dim': (32,0,1344,768), 'frames':(17,5)},
 }
 """
 External plugins may add additional formats to nodes.VHSLoadFormats
